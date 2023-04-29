a92d6b4ee4c20b2932553dd43506e2e0a2a2f356
NutsaGabshidze <126345681+NutsaGabshidze@users.noreply.github.com>
2023-04-29 11:53:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let number = 100;
 
 if (number < 50) {
-  console.log("naklebia 50ze");
+  console.log("less than 50");
 } else if (number > 20) {
-  console.log("metia 20ze");
+  console.log("more than 20");
 } else {
   console.log("error");
 }
